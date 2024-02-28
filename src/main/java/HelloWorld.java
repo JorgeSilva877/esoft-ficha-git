d75ee6778b3c7157daeea861229ad365069421b7
diogomoreira09 <diogomoreira.35.16@gmail.com>
2024-02-28 13:10:02 +0000

    adicionar o meu nome

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 public class HelloWorld extends JFrame {
     private JPanel panel1;
     private JLabel lblMensagem;
+    private JLabel lblMnesagem2;
+
     public HelloWorld(){
         setContentPane(panel1);
         pack();
